bfe409644ed3b094c6d901a2037f70164a2b5813
beng <whysguy52@gmail.com>
2019-03-30 23:53:19 -0500

    clickable node map

--- src/hax_it.cr
+++ src/hax_it.cr
@@ -4,19 +4,58 @@ require "./objects/*"
 
 LibRay.set_config_flags LibRay::FLAG_WINDOW_RESIZABLE
 LibRay.init_window 640, 480, "Hax It"
-node = Node.new
+nodes = [] of Node
+node_position_x = 0
+node_position_y = 0
+section_size = LibRay.get_screen_width / 8
+center_offset = section_size / 2
+
+nodes = Array.new(6) do |i|
+  node_position_y = i * section_size + center_offset
+  Array.new(7) do |j|
+    node_position_x = j * section_size + center_offset
+    if i % 2 == 1
+      node_position_x += center_offset
+    end
+    Node.new(node_position_x, node_position_y)
+  end
+end
+
+nodes[0][0].change_state(4)
 
 while !LibRay.window_should_close?
   # movement
   delta_t = LibRay.get_frame_time
+  did_click?(nodes)
 
   # draw
   LibRay.begin_drawing
   LibRay.clear_background LibRay::BLACK
   LibRay.draw_fps(0, 0)
-  node.draw
+  nodes.each do |rows|
+    rows.each do |node|
+      node.draw
+    end
+  end
+  # node.draw
 
   LibRay.end_drawing
+end
+
+def did_click?(nodes)
+  mouse_x = LibRay.get_mouse_x
+  mouse_y = LibRay.get_mouse_y
+  nodes.each do |rows|
+    rows.each do |node|
+      if mouse_x > node.position_x - 10 && mouse_x < node.position_x + 10
+        if mouse_y > node.position_y - 10 && mouse_y < node.position_y + 10
+          if LibRay.mouse_button_pressed?(LibRay::MOUSE_LEFT_BUTTON)
+            node.change_state
+          end
+        end
+      end
+    end
+  end
 end
 
 LibRay.close_window
--- src/objects/node.cr
+++ src/objects/node.cr
@@ -1,11 +1,40 @@
 class Node
-  property color
+  property color : LibRay::Color
+  property state : Int32
+  property position_x
+  property position_y
 
-  def initialize
-    @color = LibRay::WHITE
+  def initialize(@position_x : Int32, @position_y : Int32)
+    @color = LibRay::GRAY
+    @state = 0
+  end
+
+  def change_state
+    @state += 1
+    set_color(@state)
+  end
+
+  def change_state(value : Int32)
+    @state = value
+    set_color(@state)
+  end
+
+  def set_color(state)
+    case state
+    when 0
+      @color = LibRay::GRAY
+    when 1
+      @color = LibRay::ORANGE
+    when 2
+      @color = LibRay::RED
+    when 3
+      @color = LibRay::GREEN
+    when 4
+      @color = LibRay::BLUE
+    end
   end
 
   def draw
-    LibRay.draw_circle(20, 20, 5, color)
+    LibRay.draw_circle(position_x, position_y, 10, color)
   end
 end
